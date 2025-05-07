28afccc3d678bbbf5e3dd15bb6a543b9f5ff9d52
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-05-07 10:57:16 +0330

    Make WeatherContext

---
+++ src/Contexts/WeatherContext.tsx
@@ -0,0 +1,53 @@
+import React, {
+    useState,
+    useEffect,
+    ReactNode,
+    createContext,
+    useCallback,
+  } from "react";
+  import { getCurrentWeather } from "../services/Axios/Requests/weatherService";
+  import { WeatherContextType } from "./WeatherContext.type";
+  import { useTranslation } from "react-i18next";
+  
+  const WeatherContext = createContext<WeatherContextType>({
+    city: "",
+    setCity: () => {},
+    weatherData: null,
+    loading: false,
+  });
+  
+  export const WeatherProvider = ({
+    children,
+  }: {
+    children: ReactNode;
+  }): React.JSX.Element => {
+    const { i18n } = useTranslation();
+    const [city, setCity] = useState("Tehran");
+    const [weatherData, setWeatherData] = useState(null);
+    const [loading, setLoading] = useState(false);
+  
+    const fetchWeather = useCallback(async () => {
+      try {
+        setLoading(true);
+        const data = await getCurrentWeather(city);
+        setWeatherData(data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }, [city, i18n.language]);
+  
+    useEffect(() => {
+      fetchWeather();
+    }, [fetchWeather]);
+  
+    return (
+      <WeatherContext.Provider value={{ city, setCity, weatherData, loading }}>
+        {children}
+      </WeatherContext.Provider>
+    );
+  };
+  
+  export { WeatherContext };
+  
